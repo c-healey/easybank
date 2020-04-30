424d61ddf9f568e263be886d64581075d9499960
Cathy <cathy.healey@gmail.com>
2020-04-30 12:58:33 -0400

    Updates

---
+++ precache-manifest.7f5eabde71a412120c712b982c123ae3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c65e9288e58b2627d88c7fc27aba828",
+    "url": "/easybank/index.html"
+  },
+  {
+    "revision": "0e21c03623a1dda4a2cf",
+    "url": "/easybank/static/css/main.841fa360.chunk.css"
+  },
+  {
+    "revision": "be05c2de6de26e8fdb8e",
+    "url": "/easybank/static/js/2.255e1499.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/easybank/static/js/2.255e1499.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e21c03623a1dda4a2cf",
+    "url": "/easybank/static/js/main.187bdad9.chunk.js"
+  },
+  {
+    "revision": "62cc416819730be33b92",
+    "url": "/easybank/static/js/runtime-main.78d8ff86.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e9bde3b2ab1cce0e0050a00b69417d88.js"
+  "/easybank/precache-manifest.7f5eabde71a412120c712b982c123ae3.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/easybank/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
